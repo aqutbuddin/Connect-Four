56996ebaed2c448f10b9f62ccfcaa25c98733dc0
Aima Qutbuddin <132079324+aqutbuddin@users.noreply.github.com>
2024-09-17 15:03:16 -0500

    Update main (1).cpp

--- main (1).cpp
+++ main (1).cpp
@@ -1,13 +1,13 @@
 // Project 6: Connect 4 Game with Backtracking 
 // Aima Qutbuddin 
 // Date: 12/01/2023
-// Professor Theys, Fall 2023, 11 am Lecture
-// Enviorment: Replit and Zybooks 
-// In this project, we added to the functional connect 4 game we created in project 5. 
+// Professor M.T., Fall 2023  
+// Environment: Replit and Zybooks 
+// In this project, we added to the functional Connect 4 game we created in Project 5. 
 // Specifically, I added a function that outputs all the moves created by the players upon the user input of a 'P' or 'p'.  
 // I also added a function that undoes the most recent move made by the user upon the user input of a 'U' or 'u'.
-// In order to create aand correclty execute these funcitons, I utilized a class and implemented a linked lsit to store the moves of each player.  
-// Pass by reference allowed me to manipulate the baords and pointers assoaciated with each node. 
+//To create and correctly execute these functions, I utilized a class and implemented a linked list to store the moves of each player.  
+// Pass by reference allowed me to manipulate the boards and pointers associated with each node. 
 
 // Header Files 
 #include <iostream>
@@ -17,8 +17,8 @@ using namespace std; // utilized to avoid the std repetition for all statements
 
 class gameBoard{ // creating a class, 
     public: // public accessor
-    char board[6][7]; // 2D arrray is the data type stored to node 
-    gameBoard* next; // pointer called next, used to point to the next node in list 
+    char board[6][7]; // 2D array is the data type stored to node 
+    gameBoard* next; // pointer called next, used to point to the next node in the list 
 
     gameBoard(){ //default constructor that builds the initial board
         for (int row = 0; row < 6; row++){ // nested loop to build the board with rows and columns 
@@ -29,7 +29,7 @@ class gameBoard{ // creating a class,
     }
  
     // class method
-    void displayBoard(){ // display function for the output after each update to the game baord 
+    void displayBoard(){ // display function for the output after each update to the game board 
         cout <<"\t0 1 2 3 4 5 6"<< endl; 
         for(int row = 0; row < 6; row++){ // nested loop for the columns and rows of the gameboard
             cout<<"\t";
@@ -55,8 +55,8 @@ class gameBoard{ // creating a class,
 };
 
 // function that makes the move on the board and  stores a copy to a node of the linked list 
-gameBoard* makePlay(char column, gameBoard*& head, char &turn){ // parameters for the function, notice pas by reference to direclty manipulate the board
-    // changing the char to an integer to utlize the column value as an integer
+gameBoard* makePlay(char column, gameBoard*& head, char &turn){ // parameters for the function, notice pas by reference to directly manipulate the board
+    // changing the char to an integer to utilize the column value as an integer
     int numColumnPiece = int(column - '0');
     int row;
 
@@ -71,36 +71,36 @@ gameBoard* makePlay(char column, gameBoard*& head, char &turn){ // parameters fo
     for (row = 5; row >= 0; row--) { // looping through the rows 
         if (updatedBoard->board[row][numColumnPiece] == '-') {
             updatedBoard->board[row][numColumnPiece] = turn; // making the move on the new copy of board
-            break; //exit loop once move is made
+            break; //exit loop once the move is made
         }
     }
-    // creating a linnk between the new board to the previous one
-    // pushing in nodes through the head of the list, newest node added becomes the head of the list 
+    //Creating a link between the new board to the previous one
+    // pushing in nodes through the head of the list, the newest node added becomes the head of the list 
     updatedBoard->next = head;
-    head = updatedBoard; // head is reassigned to the most up to date board
+    head = updatedBoard; // head is reassigned to the most up-to-date board
 
-    return head;// returning the most updated board as the head board
+    return head;// returning the most updated board as the headboard
 }
-// function that outputs all the moves made by the users until the pint of a user input of 'P' or 'p'
-void PrintMovesMade(gameBoard* head){ //pointer, to manitpulate the board assocaited with the pointer 
-    if(head == nullptr){ // conditinoal chekcing if the head pointer is null
+// function that outputs all the moves made by the users until the point of a user input of 'P' or 'p'
+void PrintMovesMade(gameBoard* head){ //pointer, to manipulate the board associated with the pointer 
+    if(head == nullptr){ // conditional checking if the head pointer is null
         return;
     }
     PrintMovesMade(head->next); // recursive all to output the boards in order of no move made, to most updated board
-    head->displayBoard(); // actually displaying the head board assoicaied with the head node. 
+    head->displayBoard(); // actually displaying the headboard associated with the head node. 
     cout<<endl;
 }
 //function that undoes the most current move made by reassigning the head 
 gameBoard* undoMove(gameBoard*& head){ 
     if(head->next == nullptr){ // condition checking if the next pointer is null, 
         gameBoard emptyBoard; 
-        emptyBoard.displayBoard(); // displaying and emppty board, when the nex tpinter goes to null
+        emptyBoard.displayBoard(); // displaying an empty board, when the next pointer goes to null
         return nullptr; // no more undo moves to be made 
     }
 
-    gameBoard* tempBoard = head; // create temporary baord and store the head 
-    head = head->next; // assigning the head to the board previous to it, wiht one les move than the most updated board 
-    delete tempBoard; // rmeoving the temprary board holding the previously most updated baord 
+    gameBoard* tempBoard = head; // create temporary board and store the head 
+    head = head->next; // assigning the head to the board previous to it, with one less move than the most updated board 
+    delete tempBoard; // removing the temporary board holding the previously most updated board 
 
     if (head != nullptr){ // when the head is not null condition being checked 
         head->displayBoard(); // display the board with a move undone
@@ -110,17 +110,17 @@ gameBoard* undoMove(gameBoard*& head){
 }
 //function checking if all the dashes of the column have been populated with a turn, so an X or an O
 bool columnFull(int column, gameBoard* head){
-    for (int row = 5 ; row >= 0 ; row--){ //looping thorugh all the rows of that column 
-        if (head->board[row][column] == '-'){ // when dash detected the colunm  is not full
+    for (int row = 5 ; row >= 0 ; row--){ //looping through all the rows of that column 
+        if (head->board[row][column] == '-'){ // when dash detected the column  is not full
             return false; // false, there is still an available spot 
         }
     }
     return true; // true, no spots available, column full
 }
 //function checking the horizontal win, 
-// making sure that 4 spaces within a row are allt he same piece either X or O
+// making sure that 4 spaces within a row are all the same piece either X or O
 bool checkHorizontalWin(gameBoard* head, char turn){
-    for(int row = 5; row >= 0; row--){ //decrementaing to start checking on the upper side of the baord
+    for(int row = 5; row >= 0; row--){ //decrementing to start checking on the upper side of the board
         for(int column = 0; column < 4; column++){ // checking through the spots next to a piece 
             if(head->board[row][column] == turn && 
                 head->board[row][column + 1] == turn && 
@@ -135,7 +135,7 @@ bool checkHorizontalWin(gameBoard* head, char turn){
 //function checking for the  vertical win, 
 // making sure that 4 spaces within a column are all the same piece either X or O
 bool checkVerticalWin(gameBoard* head, char turn){
-    for(int row = 5; row >= 3; row--){ //decrementaing to start checking on the upper side of the baord
+    for(int row = 5; row >= 3; row--){ //decrementing to start checking on the upper side of the board
         for(int column = 0; column < 7; column++){ 
             if(head->board[row][column] == turn && // checking through the spots above a piece
             head->board[row - 1][column] == turn && 
@@ -150,7 +150,7 @@ bool checkVerticalWin(gameBoard* head, char turn){
 //function checking for the diagonal win, (bottom left corner to top right corner)
 // making sure that 4 spaces diagonal to a piece are all the same piece either X or O
 bool checkDiagonalWin1(gameBoard* head, char turn){
-    for (int row = 5; row >= 3; row--) { //decrementaing to start checking on the upper side of the board
+    for (int row = 5; row >= 3; row--) { //decrementing to start checking on the upper side of the board
         for (int col = 0; col < 4; col++) {
             if (head->board[row][col] == turn && //checking the piece above and to the left, 4 in a diagonal row 
                 head->board[row - 1][col + 1] == turn &&
@@ -165,7 +165,7 @@ bool checkDiagonalWin1(gameBoard* head, char turn){
 //function checking for the diagonal win, (bottom right corner to top left corner)
 // making sure that 4 spaces diagonal to a piece are all the same piece either X or O
 bool checkDiagonalWin2(gameBoard* head, char turn){
-    for (int row = 5; row >= 3; row--) { //decrementaing to start checking on the upper side of the board
+    for (int row = 5; row >= 3; row--) { //decrementing to start checking on the upper side of the board
         for (int col = 0; col < 4; col++) {
             if (head->board[row][col] == turn && //checking the piece below and to the right, 4 in a diagonal row 
                 head->board[row - 1][col - 1] == turn &&
@@ -179,10 +179,10 @@ bool checkDiagonalWin2(gameBoard* head, char turn){
 }
 //function searches for a draw by looking to see if each spot is populated with a piece, either X or O
 bool boardFull(gameBoard* head){
-    for(int row = 0; row < 6; row++){ //looping throught the 2D array
+    for(int row = 0; row < 6; row++){ //looping through the 2D array
         for(int column = 0; column< 7; column++){ 
-            if(head->board[row][column] == '-'){ // when dash detected the baord is not full
-                return false; //false, baord is not full 
+            if(head->board[row][column] == '-'){ // when dash detected the board is not full
+                return false; //false, board is not full 
             }
         }
     }
@@ -190,8 +190,8 @@ bool boardFull(gameBoard* head){
 }
 int main(){
     
-    // intializing variables, specifically the columnPiece, the gameBoard, and 
-    // the turn of the current player --> alway set to 'X' first
+    // initializing variables, specifically the column piece, the gameBoard, and 
+    // the turn of the current player --> always set to 'X' first
     char columnPiece; 
     char playerTurn = 'X'; 
 
@@ -200,39 +200,39 @@ int main(){
     gameBoard* head = nullptr;
     
     while(true){ //gameplay loop 
-        // prompting for input with cout statement and recieving input with cin statement
+        // prompting for input with count statement and receiving input with cin statement
         cout << "It is " << playerTurn << "'s turn." << endl;
         cout << "Enter a column to place your piece.";
         cin >> columnPiece;
         //cout << endl; 
 
-        if (columnPiece == 'Q' || columnPiece == 'q'){ // in the case where user would like to quit the game
-            break; // break out of loop, no more gameplay 
+        if (columnPiece == 'Q' || columnPiece == 'q'){ // in the case where the user would like to quit the game
+            break; // break out of the loop, no more gameplay 
         }
         else if(columnPiece == 'P' || columnPiece == 'p'){ // in the case where the user wants to print all the moves made
             PrintMovesMade(head);  // calling function
-            break;// break out of loop, no more gameplay 
+            break;// break out of the loop, no more gameplay 
         }
         else if(columnPiece == 'U' || columnPiece == 'u'){ // in the case where the user wants to undo a move 
             head = undoMove(head); // calling function, assigning to head
-            if (head!= nullptr){  // when the head not null, switch the user becasue undo counts as a play 
+            if (head!= nullptr){  // when the head not null, switch the user because undo counts as a play 
                 playerTurn = (playerTurn == 'X') ? 'O' : 'X';
             }
             continue;// continue gameplay 
         }
 
-        if(isdigit(columnPiece)){ // chekcing if the user entered char is a digit or not 
+        if(isdigit(columnPiece)){ // checking, if the user entered char, is a digit or not 
             int columnInput = int(columnPiece - '0'); // changing the char to an integer 
             // checking integer falls between the bounds of an acceptable column window 
             if (columnInput >= 0 && columnInput < 7){ 
-                // making sure there is room within their chosen column for move to be made 
+                // making sure there is room within their chosen column for a move to be made 
                 if(head!=nullptr){
                     if(columnFull(columnInput, head)){ 
                         cout << "column chosen is already full" << endl;
-                        continue; // break out of current iteration and start the next 
+                        continue; // break out of the current iteration and start the next 
                     }
                 }
-                //calling function to make move and update the board with the board that has the move made 
+                //calling function to make moves and update the board with the board that has the move made 
                 if(head == nullptr){ // when condition met, update the initial board pointer
                     head = makePlay(columnPiece, initialBoard, playerTurn);
                 }
@@ -243,7 +243,7 @@ int main(){
             }
             else{ // prompting whne iinvalid column number entered 
                 cout << "Please enter a valid column." << endl; 
-                continue; // break out of current iteration and start the next
+                continue; // break out of the current iteration and start the next
             }
         }
         // when conditions for any type of win( horizontal,vertical, or 1 of the 2 diagonals) are met
@@ -253,15 +253,15 @@ int main(){
             checkDiagonalWin2(initialBoard,playerTurn) == true){
             //congratulatory statement outputted 
             cout << "Game is Over, Player " << playerTurn << " got 4 in a row!!!! " << endl;
-            break; // break out of loop, no more gameplay 
+            break; // break out of the loop, no more gameplay 
         }
-        else if (boardFull(initialBoard) == true){ // when condition for a draw between palyers is detected
+        else if (boardFull(initialBoard) == true){ // when condition for a draw between players is detected
             // tie, no winner, statement outputted 
             cout << "Board is Full, It's a Draw!!!" << endl;
-            break;// break out of loop, no more gameplay 
+            break;// break out of the loop, no more gameplay 
         }
         else { // switching turns so gameplay can continue 
-            // when the current player is X is true, it switches to O. if it is false, then it stays X 
+            //When the current player is X is true, it switches to O. If it is false, then it stays X 
             playerTurn = (playerTurn == 'X') ? 'O' : 'X'; 
         }
     }
